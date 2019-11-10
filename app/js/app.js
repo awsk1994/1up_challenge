a09c6a349b15be0934d7e4f2be334d1e09594090
Alex Wong <awsk1994@gmail.com>
2019-11-09 20:53:07 -0500

    restructure to fix controler error

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var myApp = angular.module('myApp', ['ngRoute'])
-    .config(function($routeProvider, $locationProvider){
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider, $locationProvider){
        $routeProvider.when('/main',
            {
                templateUrl: 'templates/main.html'
@@ -9,13 +10,39 @@ var myApp = angular.module('myApp', ['ngRoute'])
         $routeProvider.when('/view1',
             {
                 templateUrl: 'templates/view1.html',
-                controller: 'view1Controller'
+                controller: 'View1Controller'
             });
         $routeProvider.when('/view2',
             {
                 templateUrl: 'templates/view2.html',
-                controller: 'view2Controller'
+                controller: 'View2Controller'
             });
         $routeProvider.otherwise({redirectTo: '/main'});
         $locationProvider.html5Mode(true);
     });
+
+myApp.controller('View1Controller', function($scope, $location, $http) {
+    $scope.client_id = "f8d5957efd46e181c27676ae340c74";
+    $scope.client_secret = "Wfonj5RqcKZIsEu3fnJVXajkMsTJp4mN";
+
+    $scope.create_user = function(app_user_id, client_id, client_secret){
+        let url = "https://api.1up.health/user-management/v1/user"
+        let data = {
+            "app_user_id": app_user_id,
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+
+        $http.post(url, data).then(function(res){
+            console.log("response");
+            console.log(res);
+        });
+    };
+
+    $scope.create_user(0, $scope.client_id, $scope.client_secret);
+});
+
+myApp.controller('View2Controller', function($scope, $location) {
+
+
+});
--- app/js/controllers/View1Controller.js
+++ app/js/controllers/View1Controller.js
@@ -1,5 +1,5 @@
-'use strict';
-
-myApp.controller('View1Controller',
-    function ProjectsController($scope, $location) {
-});
+//'use strict';
+//
+//myApp.controller('View1Controller',
+//    function ProjectsController($scope, $location) {
+//});
