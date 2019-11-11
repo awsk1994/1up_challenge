8a61dbebe1644f40f8427e6acdcc831253ac3c0b
Alex Wong <awsk1994@gmail.com>
2019-11-11 09:52:30 -0500

    access fhir data, generate patietn data and fetch patietn data

--- app/js/app.js
+++ app/js/app.js
@@ -22,15 +22,18 @@ myApp.config(function($routeProvider, $locationProvider, $httpProvider){
         $locationProvider.html5Mode(true);
     });
 
-myApp.controller('View1Controller', function($scope, $location, $http) {
-    $scope.cred_type = "term_ie";    // "heroku" or "local" or "term_ie"
+myApp.controller('View1Controller', function($scope, $location, $http, $window) {
+    $scope.cred_type = "local";    // "heroku" or "local" or "term_ie"
     $scope.users = {};
     $scope.create_user_resp = null;
     $scope.cache_access_code = null;
     $scope.system_id_resp = null;
     $scope.access_token = null;
     $scope.refresh_token = null;
+    $scope.patient_id = null;
+    $scope.get_patient_resp = null;
 
+    $scope.create_patient_err = null;
 
     // TODO: Should store client_id and secret in encrypted or hashed version
     $scope.localhost_cred = {
@@ -121,7 +124,7 @@ myApp.controller('View1Controller', function($scope, $location, $http) {
     $scope.get_access_token = function(app_user_id){
         let client_info = get_client_info();
 
-        function get_access_code(code){
+        function retrieve_access_code(code){
             let url = "https://api.1up.health/fhir/oauth2/token";
             let data = {
                 "client_id": client_info.client_id,
@@ -146,12 +149,73 @@ myApp.controller('View1Controller', function($scope, $location, $http) {
 
             $http.post(url, {}, {"params": data}).then(function(res){
                 let code = res.data.code
-                get_access_code(code);
+                retrieve_access_code(code);
             });
         }
 
         get_auth_code(app_user_id);
     }
+
+    $scope.redirectToEpicLogin = function(){
+        // username: fhirjason, pw: epicepic1
+        let system_id = 4706;
+        let url = "https://quick.1up.health/connect/" + system_id + "?client_id="+get_client_info().client_id+"&access_token=" + $scope.access_token;
+        $window.open(url, '_blank');
+    };
+
+    $scope.access_client_data = function(){
+        let client_info = get_client_info();
+        let url = "https://api.1up.health/connect/system/clinical"
+        let params = {
+            "clientid": client_info.client_id,
+            "clientsecret": client_info.client_secret
+        }
+        $http.get(url, params).then(function(res){
+            console.log(res.data);
+        })
+    }
+
+    $scope.create_patient_data = function(id, gender){
+        let url = "https://api.1up.health/fhir/dstu2/Patient";
+        let config = {
+            "headers": {
+                "Authorization": "Bearer " + $scope.access_token
+            }
+        };
+        let data = {
+            "resourceType": "Patient",
+            "id": id,
+            "gender": gender
+        }
+        $http.post(url, data, config).then(function(res){
+            $scope.create_patient_err = null;
+            console.log(res.data);
+            if(res.data.issue){
+                $scope.create_patient_err = res.data.issue[0].details.text
+            } else {
+                $scope.patient_id = res.data.id;
+            }
+        })
+    };
+
+    $scope.get_patient_data = function(patient_id){
+        let url = "https://api.1up.health/fhir/dstu2/Patient/" + patient_id + "/$everything";
+        let config = {
+            "headers": {
+                "Authorization": "Bearer " + $scope.access_token
+            }
+        }
+
+        $http.get(url, config).then(function(res){
+            console.log(res.data);
+            $scope.get_patient_resp = res.data;
+        })
+    }
+
+    $scope.generate_sample_get_patient_data = function(){
+        $scope.get_patient_resp = {"resourceType":"Bundle","type":"searchset","total":2,"entry":[{"fullUrl":"https://api.1up.health/fhir/dstu2/Patient/730bea4ff16f","search":{"mode":"match","score":21.654974},"resource":{"gender":"male","meta":{"lastUpdated":"2019-11-11T14:36:44.501Z","versionId":"9000000000000"},"id":"730bea4ff16f","resourceType":"Patient"}},{"fullUrl":"https://api.1up.health/fhir/dstu2/Provenance/730bea4ff16f366cba5e2c85f25d75e1148d35f3b408","search":{"mode":"match","score":26.93042},"resource":{"agent":[{"role":{"system":"http://hl7.org/fhir/provenance-participant-role","code":"author"},"userId":{"system":"https://1up.health/dev/codesystems/user-id","value":"105544"}},{"actor":{"reference":"Device/software"},"role":{"system":"http://hl7.org/fhir/v3/ParticipationType","code":"DEV"},"userId":{"system":"https://1up.health/dev/codesystems/oneup-service-id","value":"fhirapiserver"}}],"activity":{"coding":[{"code":"new","system":"http://hl7.org/fhir/v3/ActStatus","display":"new"}]},"recorded":"2019-11-11T14:36:44.530Z","target":[{"reference":"Patient/730bea4ff16f/_history/9000000000000"}],"meta":{"lastUpdated":"2019-11-11T14:36:44.573Z","versionId":"9000000000000"},"id":"730bea4ff16f366cba5e2c85f25d75e1148d35f3b408","entity":[{"reference":"Patient/730bea4ff16f/_history/9000000000000","role":"source","type":{"code":"Patient","system":"http://hl7.org/fhir/definition-resource-types"}}],"resourceType":"Provenance"}}]}
+    }
+
 });
 
 myApp.controller('View2Controller', function($scope, $location) {
